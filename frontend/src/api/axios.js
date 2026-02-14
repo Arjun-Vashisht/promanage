f43790438b2c3d29629e5909c95799650e314cef
Arjun Vashisht <arjun@eventgraphia.com>
2026-02-14 05:46:25 +0530

    redirection to login page

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -4,15 +4,32 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
 
-// Attach token to every request
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("accessToken");
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response,
+
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("refreshToken");
+
+      window.location.href = "/login";
+    }
+
+    return Promise.reject(error);
   }
-
-  return config;
-});
+);
 
 export default api;
