8d1a663747f9a9e818f578f47272e25b42097674
Arjun Vashisht <arjun@eventgraphia.com>
2026-03-14 21:09:28 +0530

    allow hosts of render

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,7 +32,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = [
+    "promanage-backend-6suz.onrender.com"
+]
 
 
 # Application definition
